392f80983d060edaeaaa9781663a65654d157d60
Geoff P <geoffperrin@gmail.com>
2017-09-05 15:09:51 -0500

    adding tableau time filter

---
+++ R_scripts/shiny_apps.R
@@ -0,0 +1,9 @@
+install.packages('rsconnect')
+
+rsconnect::setAccountInfo(name='geoffperrin',
+                          token='DA391E8CAF13D48A7D82BC947396EAC8',
+                          secret='1mPyXApVv919TA/DPxJW6L9u5cLr0F6/bxXaowxU')
+
+
+library(rsconnect)
+#rsconnect::deployApp('path/to/your/app')
